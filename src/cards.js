55442eb06133e48948b9f3f052eb343431da175b
Julio Cesar <juliocnsilva.28@gmail.com>
2024-11-15 11:14:25 -0300

    ChamadaBackendMenu

--- src/cards.js
+++ src/cards.js
@@ -1,19 +1,24 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function Cards({title, text, imageSrc, buttonLabel, buttonAction}) {
-    return(
+function Cards({ titulo, descricao, preco, imagem, buttonLabel, buttonAction }) {
+    return (
         <div>
             <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={imageSrc} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>{text}</Card.Text>
-                <Button variant="danger" onClick={buttonAction}>{buttonLabel}</Button>
-            </Card.Body>
+                <Card.Img variant="top" src={imagem} alt={titulo} />
+                <Card.Body>
+                    <Card.Title>{titulo}</Card.Title>
+                    <Card.Text>
+                        {descricao} <br />
+                        <strong>{preco}</strong>
+                    </Card.Text>
+                    <Button variant="danger" onClick={buttonAction}>
+                        {buttonLabel}
+                    </Button>
+                </Card.Body>
             </Card>
         </div>
- );
+    );
 }
 
 export default Cards;
